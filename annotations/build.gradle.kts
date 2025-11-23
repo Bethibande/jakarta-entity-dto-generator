c7c1c53f3fb1a5ca129d5f3faa1734dcce0e92cf
bethibande <bethibande@gmail.com>
2025-11-23 10:50:09 +0100

    Set up publishing #2

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     id("java")
+    `java-library`
+    `maven-publish`
+    signing
 }
 
 group = "de.bethibande.process"
-version = "unspecified"
+version = "1.0"
+
+description = "Annotations for DTO generation"
 
 repositories {
     mavenCentral()
@@ -17,4 +22,72 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+
+            pom {
+                name = project.name
+                description = project.description
+
+                url = "https://github.com/Bethibande/jakarta-entity-dto-generator"
+
+                licenses {
+                    license {
+                        name = "Apache-2"
+                        url = "https://github.com/Bethibande/jakarta-entity-dto-generator/blob/master/LICENSE"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "bethibande"
+                        name = "Max Bethmann"
+                        email = "contact@bethibande.com"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git://github.com/Bethibande/jakarta-entity-dto-generator.git"
+                    developerConnection = "scm:git:ssh://github.com/Bethibande/jakarta-entity-dto-generator.git"
+                    url = "https://github.com/Bethibande/jakarta-entity-dto-generator"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Maven-Releases"
+            url = uri("https://pckg.bethibande.com/repository/maven-snapshots/")
+            credentials {
+                if (providers.gradleProperty("mavenUsername").isPresent) {
+                    username = providers.gradleProperty("mavenUsername").get()
+                    password = providers.gradleProperty("mavenPassword").get()
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
 }
